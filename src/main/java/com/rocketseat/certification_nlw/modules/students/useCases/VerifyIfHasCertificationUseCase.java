763d358de727c35514e6c5d6baaf262dfee575eb
alic <alic.victor@academico.ifpb.edu.br>
2024-02-07 20:27:12 -0300

    Aula 02

--- src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,13 +1,18 @@
 package com.rocketseat.certification_nlw.modules.students.useCases;
 
 import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.repositories.CertificationStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
     public boolean execute(VerifyHasCertificationDTO dto) {
-        if(dto.getEmail().equals("alic@gmail.com") && dto.getTechnology().equals("Java")) {
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if (!result.isEmpty()) {
             return true;
         }
         return false;
